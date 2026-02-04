42bddc3ea837268538194e9371d1c076297cdfe8
Roman <m3f3dr0nbristleback@gmail.com>
2026-02-04 20:06:28 +0300

    fix

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"go-chat/handlers"
 	"go-chat/room"
 	"net/http"
+	"os"
 )
 
 func main() {
 	manager := room.NewManager()
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("Server running on port", port)
+
 	http.HandleFunc("/create", handlers.CreateRoomHandler(manager))
 	http.HandleFunc("/ws", handlers.WebSocketHandler(manager))
 
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.ListenAndServe(":8080", nil)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		fmt.Println("Server error:", err)
+	}
 }
